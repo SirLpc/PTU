5bdce64818dccc47ae295ddbb89e73ba8b8bc731
petchanliu <petchanliu@tencent.com>
2022-08-31 09:06:42 +0800

    debug bug

--- Assets/Examples/05_Typescript/TsQuickStart.cs
+++ Assets/Examples/05_Typescript/TsQuickStart.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using Puerts;
 using System;
+using System.IO;
 
 //typescript工程在TsProj，在该目录执行npm run build即可编译并把js文件拷贝进unity工程
 
@@ -15,7 +16,10 @@ namespace PuertsTest
         {
             if (debug)
             {
-                jsEnv = new JsEnv(new DefaultLoader("D:/UnityPrograms/PTU/TsProj/output"), 8080);
+                var dir = new DirectoryInfo(Application.dataPath);
+                var path = Path.Combine(dir.Parent.FullName, "TsProj/output/");
+                Debug.Log(path);
+                jsEnv = new JsEnv(new DefaultLoader(path), 8080);
                 jsEnv.WaitDebugger();
             }
             else
--- TsProj/QuickStart.ts
+++ TsProj/QuickStart.ts
@@ -31,22 +31,22 @@ import { TestGame } from './src/Sample/TestNewEngine/TestGame'
 // }, 1000);
 
 // --------------My component system test
-DIC.Register(Logger, function():Logger { return new UnityDebugLogger(); });
-DIC.Register(TSComponentHub, function():TSComponentHub { return new TSComponentHub(); });
-DIC.Register(CubeGO, function():CubeGO { return new CubeGO(DIC.Make(UnityGameObjectComponent), DIC.Make(Logger)); })
-DIC.Register(TSScene, function():TSScene { return new TSScene(); })
-DIC.Register(UnityGameObjectComponent, function():UnityGameObjectComponent {
-        let go = UnityEngine.GameObject.CreatePrimitive(UnityEngine.PrimitiveType.Cube);
-        return new UnityGameObjectComponent(go);
-    })
-DIC.Register(App, function():App {
-    return new GameApp(DIC.Make<TSComponentHub>(TSComponentHub), DIC.Make<TSScene>(TSScene), DIC.Make<CubeGO>(CubeGO));
- });
-DIC.Make<App>(App).Start();
+// DIC.Register(Logger, function():Logger { return new UnityDebugLogger(); });
+// DIC.Register(TSComponentHub, function():TSComponentHub { return new TSComponentHub(); });
+// DIC.Register(CubeGO, function():CubeGO { return new CubeGO(DIC.Make(UnityGameObjectComponent), DIC.Make(Logger)); })
+// DIC.Register(TSScene, function():TSScene { return new TSScene(); })
+// DIC.Register(UnityGameObjectComponent, function():UnityGameObjectComponent {
+//         let go = UnityEngine.GameObject.CreatePrimitive(UnityEngine.PrimitiveType.Cube);
+//         return new UnityGameObjectComponent(go);
+//     })
+// DIC.Register(App, function():App {
+//     return new GameApp(DIC.Make<TSComponentHub>(TSComponentHub), DIC.Make<TSScene>(TSScene), DIC.Make<CubeGO>(CubeGO));
+//  });
+// DIC.Make<App>(App).Start();
 
 // ---------------TSEngine test
-// let engine = new Engine();
-// engine.start(new TestGame());
+let engine = new Engine();
+engine.start(new TestGame());
 
 
 // let iv : Puergp.Variables.IntVariable = UnityEngine.Resources.Load("IntVariable") as  Puergp.Variables.IntVariable;
