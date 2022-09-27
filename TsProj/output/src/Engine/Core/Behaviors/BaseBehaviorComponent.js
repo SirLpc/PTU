dbe0e95d6a2e4b2a944812aab015e4095784c8d7
sirlpc <sirlpc123@gmail.com>
2022-09-27 23:24:09 +0800

    onenable ondisable

--- TsProj/output/src/Engine/Core/Behaviors/BaseBehaviorComponent.js
+++ TsProj/output/src/Engine/Core/Behaviors/BaseBehaviorComponent.js
@@ -3,6 +3,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.BaseBehaviorComponent = void 0;
 const CommonComponent_1 = require("../Components/CommonComponent");
 class BaseBehaviorComponent extends CommonComponent_1.CommonComponent {
+    _enabled = false;
+    get enabled() {
+        return this._enabled;
+    }
+    set enabled(val) {
+        if (this._enabled != val) {
+            this._enabled = val;
+            if (this._enabled) {
+                this.onEnabled();
+            }
+            else {
+                this.onDisabled();
+            }
+        }
+    }
+    load() {
+        super.load();
+        this._enabled = true;
+        this.onEnabled();
+    }
+    unload() {
+        this._enabled = false;
+        this.onDisabled();
+        super.unload();
+    }
+    onEnabled() {
+    }
+    onDisabled() {
+    }
 }
 exports.BaseBehaviorComponent = BaseBehaviorComponent;
 //# sourceMappingURL=BaseBehaviorComponent.js.map
--- TsProj/src/Engine/Core/Behaviors/BaseBehaviorComponent.ts
+++ TsProj/src/Engine/Core/Behaviors/BaseBehaviorComponent.ts
@@ -3,4 +3,41 @@ import { CommonComponent } from "../Components/CommonComponent";
 
 export abstract class BaseBehaviorComponent extends CommonComponent {
 
+    private _enabled: boolean = false;
+
+    public get enabled(): boolean {
+        return this._enabled;
+    }
+
+    public set enabled(val: boolean) {
+        if (this._enabled != val) {
+            this._enabled = val;
+            if (this._enabled) {
+                this.onEnabled();
+            }
+            else {
+                this.onDisabled();
+            }
+        }
+    }
+
+    public override load(): void {
+        super.load();
+
+        this._enabled = true;
+        this.onEnabled();
+    }
+
+    public override unload(): void {
+        this._enabled = false;
+        this.onDisabled();
+
+        super.unload();
+    }
+
+    public onEnabled(): void {
+    }
+
+    public onDisabled(): void {
+    }
 }
